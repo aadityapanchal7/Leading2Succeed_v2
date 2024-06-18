c8d1b96f08a256cbfdf3713826ac39f56da406f4
wilder09 <aadityapanchal289@gmail.com>
2024-06-17 20:05:18 -0500

    removed mute attribute from video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
     <div className=''>
       {/* Video */}
       <div className=" w-full absolute h-screen z[-3] ">
-        <video src="./assets/Test_Vid.mp4" className=" overflow-hidden absolute w-full h-full object-cover z-[-2]" loop autoPlay muted />
+        <video src="./assets/Test_Vid.mp4" className=" overflow-hidden absolute w-full h-full object-cover z-[-2]" loop autoPlay />
       </div>
       
       <div className='h-screen bg-black/50'>
